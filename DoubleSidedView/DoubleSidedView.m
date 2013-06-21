2d48fb963c32cd02c17a0ed38828f01eeb4f4c99
hsusmita <hsusmita4@gmail.com>
2013-06-21 15:23:54 +0530

    Code cleaning done

--- DoubleSidedView/DoubleSidedView.m
+++ DoubleSidedView/DoubleSidedView.m
@@ -39,58 +39,6 @@
     return self;
 }
 
-/*-(id)initWithFrame:(CGRect)frame frontImage:(NSString*)frontImageName backImage:(NSString *)backImageName
-{
-    NSLog(@"Passed frame = %f %f %f %f",frame.origin.x,frame.origin.y,frame.size.width,frame.size.height);
-    CGRect containerFrame = CGRectMake(frame.origin.x - frame.size.width,
-                                       frame.origin.y,
-                                       frame.size.width*2,
-                                       frame.size.height);
-
-    NSLog(@"container frame = %f %f %f %f",containerFrame.origin.x,containerFrame.origin.y,containerFrame.size.width,containerFrame.size.height);
-    self = [super initWithFrame:containerFrame];
-    [self printFrame:self.layer withMessage:@"self"];
-    CGRect doubleLayerFrame = CGRectMake(containerFrame.size.width/2,0,containerFrame.size.width/2,frame.size.height);
-    
-//    CGRect doubleLayerFrame = CGRectMake(0,0,containerFrame.size.width,frame.size.height);
-//    CGRect imageLayerFrame = CGRectMake(0,0,containerFrame.size.width,frame.size.height);
-    self.backgroundColor = [UIColor grayColor];
-        
-        //Configuring doubleSidedLayer
-        self.doubleSidedLayer  = [CATransformLayer layer];
-        [self.doubleSidedLayer setFrame:doubleLayerFrame];
-        [self printFrame:self.doubleSidedLayer withMessage:@"DoublesidedLayer"];
-        [self printBounds:self.doubleSidedLayer withMessage:@"DoublesidedLayer"];
-        NSLog(@"Anchor Points = %f %f",self.doubleSidedLayer.anchorPoint.x, self.doubleSidedLayer.anchorPoint.y);
-        NSLog(@"position = %f %f",self.doubleSidedLayer.position.x,self.doubleSidedLayer.position.y);
-        
-        [self.doubleSidedLayer setAnchorPoint:CGPointMake(0, 0.5)];
-        [self.doubleSidedLayer setPosition:CGPointMake(self.doubleSidedLayer.position.x+self.doubleSidedLayer.bounds.size.width*(self.doubleSidedLayer.anchorPoint.x-0.5),self.doubleSidedLayer.position.y+self.doubleSidedLayer.bounds.size.height*(self.doubleSidedLayer.anchorPoint.y-0.5))];
-    
-        //Configuring top layer
-        topLayer = [[CALayer alloc]init];
-        [self.topLayer setFrame:doubleSidedLayer.bounds];
-        [self.topLayer setContents:(id)[UIImage imageNamed:frontImageName].CGImage];
-        [self.topLayer setZPosition:2];
-        [self.topLayer setDoubleSided:NO];
-        
-        //Configuring bottom layer
-        bottomLayer = [[CALayer alloc]init];
-        [self.bottomLayer setFrame:doubleSidedLayer.bounds];
-        [self.bottomLayer setContents:(id)[UIImage imageNamed:backImageName].CGImage];
-        [self.bottomLayer setZPosition:1];
-        
-        [self.doubleSidedLayer addSublayer:topLayer];
-        [self.doubleSidedLayer addSublayer:bottomLayer];
-        [self.layer addSublayer:doubleSidedLayer];
-    
-        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(turn:)];
-        [self addGestureRecognizer:tap];
-        isFlipped = NO;
-        return self;
-    
-}*/
-
 -(id)initWithFrame:(CGRect)frame frontImage:(NSString*)frontImageName backImage:(NSString *)backImageName
 {
     CGRect containerFrame = CGRectMake(frame.origin.x - frame.size.width,
